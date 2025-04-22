fc55eedbb2d89f55ce753fe7bb8a820ede23e3df
Nefari0 <pinnlab@gmail.com>
2025-04-22 13:38:52 +0300

    fix: init and nasa news

--- init.sh
+++ init.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#Новый скрипт
   rm -rf Afina_go
   apt-get install git -y
   git clone https://github.com/Nefar10/Afina_go.git
   cp /go/Afina_go/init.sh /go/init.sh
-  сd Afina_go
+  cd Afina_go
   go build -o main
   ./main -v
